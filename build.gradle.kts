45713937cc4b518ee3f90f1b687ef78be8166d1a
cuidok <zapwe@qq.com>
2023-05-04 15:55:00 +0800

    Add java psi support

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ intellij {
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) })
+
+    plugins.add("com.intellij.java")
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
